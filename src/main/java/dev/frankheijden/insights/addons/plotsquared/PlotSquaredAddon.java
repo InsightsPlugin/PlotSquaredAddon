aed2d58b1b802974dc27043f0ce517c6dec969c5
Frank van der Heijden <frank.boekanier@gmail.com>
2021-02-21 22:41:28 +0100

    Improve region computation needed when merely looking up regions

--- src/main/java/dev/frankheijden/insights/addons/plotsquared/PlotSquaredAddon.java
+++ src/main/java/dev/frankheijden/insights/addons/plotsquared/PlotSquaredAddon.java
@@ -6,15 +6,15 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 import dev.frankheijden.insights.api.addons.InsightsAddon;
 import dev.frankheijden.insights.api.addons.Region;
-import dev.frankheijden.insights.api.addons.SimpleMultiCuboidRegion;
+import dev.frankheijden.insights.api.objects.chunk.ChunkPart;
 import dev.frankheijden.insights.api.objects.math.Cuboid;
 import dev.frankheijden.insights.api.objects.math.Vector3;
 import org.bukkit.World;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PlotSquaredAddon implements InsightsAddon {
     
@@ -29,27 +29,26 @@ public class PlotSquaredAddon implements InsightsAddon {
         }
         return sb.substring(1);
     }
+
+    private Cuboid adapt(CuboidRegion region, World world) {
+        BlockVector3 min = region.getMinimumPoint();
+        BlockVector3 max = region.getMaximumPoint();
+        return new Cuboid(
+                world,
+                new Vector3(min.getX(), min.getY(), min.getZ()),
+                new Vector3(max.getX(), max.getY(), max.getZ())
+        );
+    }
     
     public Optional<Region> adapt(Plot basePlot, World world) {
         if (basePlot == null) return Optional.empty();
 
-        Set<CuboidRegion> regions = basePlot.getRegions();
-        if (regions.isEmpty()) return Optional.empty();
+        Set<CuboidRegion> cuboids = basePlot.getRegions();
+        if (cuboids.isEmpty()) return Optional.empty();
 
-        List<Cuboid> cuboids = new ArrayList<>(regions.size());
-        for (CuboidRegion region : regions) {
-            BlockVector3 min = region.getMinimumPoint();
-            BlockVector3 max = region.getMaximumPoint();
-            cuboids.add(new Cuboid(
-                    world,
-                    new Vector3(min.getX(), min.getY(), min.getZ()),
-                    new Vector3(max.getX(), max.getY(), max.getZ())
-            ));
-        }
-
-        return Optional.of(new SimpleMultiCuboidRegion(
+        return Optional.of(new PlotSquaredRegion(
+                world,
                 cuboids,
-                getPluginName(),
                 getId(basePlot.getConnectedPlots())
         ));
     }
@@ -77,5 +76,35 @@ public class PlotSquaredAddon implements InsightsAddon {
                 location.getBlockY(),
                 location.getBlockZ()
         ).getOwnedPlotAbs(), location.getWorld());
+    }
+
+    public class PlotSquaredRegion implements Region {
+
+        private final World world;
+        private final Set<CuboidRegion> cuboids;
+        private final String key;
+
+        public PlotSquaredRegion(World world, Set<CuboidRegion> cuboids, String key) {
+            this.world = world;
+            this.cuboids = cuboids;
+            this.key = key;
+        }
+
+        @Override
+        public String getAddon() {
+            return getPluginName();
+        }
+
+        @Override
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public List<ChunkPart> toChunkParts() {
+            return cuboids.stream()
+                    .flatMap(cuboid -> adapt(cuboid, world).toChunkParts().stream())
+                    .collect(Collectors.toList());
+        }
     }
 }
